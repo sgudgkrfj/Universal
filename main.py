1f79c0087c0338e9e80e1c7ce62585e519f76b28
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-06-02 10:09:39 +0300

    друге завдання

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-class InvalidUrlError(Exception):
-    def __init__(self, username):
-        self.username = username
+class InvalidPasswordError(Exception):
+    def __init__(self, password):
+        self.password = password
 
 
-def register_user(username):
-    if len(username) < 5:
-        raise InvalidUrlError(username)
+class InvalidSumbolInPassword(Exception):
+    def __init__(self, password):
+        self.password = password
+
+
+def validate_password(password):
+    if len(password) >= 8:
+        print("Пароль підходить о параметру довжини")
     else:
-        print("vas zaregano")
+        raise InvalidPasswordError(password)
 
+
+def validate_password2(password):
+    if " " in password:
+        raise InvalidSumbolInPassword(password)
+    else:
+        print("В паролі не знайдено забороненого символу пробілу")
 try:
-    username = input("Введіть ім'я користувача")
-    register_user(username)
-except InvalidUrlError as e:
-    print(f"Неправильне ім'я користувача {e.username}!"
-          f"Треба мінімум 5 символів!")
-
-
+    password=input("Введіть пароль: ")
+    validate_password(password)
+    validate_password2(password)
+except InvalidPasswordError as e:
+    print(f"не правильний пароль '{e.password}'"
+          f",треба мінімум 8 символів")
+except InvalidSumbolInPassword as d:
+    print(f"Пароль {d.password}"
+          f" не приймається через присутність забороненого символу пробіл ' '")
 
 
