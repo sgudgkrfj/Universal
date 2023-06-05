2feb799114c1543a952ac65fa7c334ca8ecd61dd
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-06-06 01:53:16 +0300

    друге завдання

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-class InvalidUrlError(Exception):
-    def __init__(self, username):
+class UserNotFoundError(Exception):
+    pass
+
+class UserDatabase:
+    def __init__(self):
+        self.users = {}
+
+    def add_user(self, username, user):
+        self.users[username] = user
+
+    def get_user(self, username):
+        if username in self.users:
+            return self.users[username]
+        else:
+            raise UserNotFoundError(f"User '{username}' not found")
+
+
+class User:
+    def __init__(self, username, email):
         self.username = username
+        self.email = email
 
+user_db = UserDatabase()
 
-def register_user(username):
-    if len(username) < 5:
-        raise InvalidUrlError(username)
-    else:
-        print("vas zaregano")
+user1 = User("john", "john@example.com")
+user2 = User("jane", "jane@example.com")
+user_db.add_user(user1.username, user1)
+user_db.add_user(user2.username, user2)
+
 
 try:
-    username = input("Введіть ім'я користувача")
-    register_user(username)
-except InvalidUrlError as e:
-    print(f"Неправильне ім'я користувача {e.username}!"
-          f"Треба мінімум 5 символів!")
-
-
-
-
+    username = input("Введіть ім'я користувача: ")
+    user = user_db.get_user(username)
+    print("Знайдений користувач:", user.username, user.email)
+except UserNotFoundError:
+    print(f"Користувача не знайдено")
