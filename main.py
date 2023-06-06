5e7b3e0542a3be081c8f15bb3a56bbd5f7b6ea06
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-06-06 03:03:45 +0300

    третє завдання

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-class InvalidUrlError(Exception):
-    def __init__(self, username):
-        self.username = username
+class TemperatureConverter:
+    @staticmethod
+    def celsius_to_fahrenheit(celsius):
+        if celsius < -273.15:  # Абсолютний нуль
+            raise ValueError("Недопустиме значення температури")
+        return celsius * 9/5 + 32
+
+    @staticmethod
+    def fahrenheit_to_celsius(fahrenheit):
+        if fahrenheit < -459.67:  # Абсолютний нуль
+            raise ValueError("Недопустиме значення температури")
+        return (fahrenheit - 32) * 5/9
 
 
-def register_user(username):
-    if len(username) < 5:
-        raise InvalidUrlError(username)
-    else:
-        print("vas zaregano")
+converter = TemperatureConverter()
 
 try:
-    username = input("Введіть ім'я користувача")
-    register_user(username)
-except InvalidUrlError as e:
-    print(f"Неправильне ім'я користувача {e.username}!"
-          f"Треба мінімум 5 символів!")
+    celsius = float(input("Введіть температуру в градусах Цельсія: "))
+    fahrenheit = converter.celsius_to_fahrenheit(celsius)
+    print(f"{celsius} градусів Цельсія = {fahrenheit} градусів Фаренгейта")
+except ValueError as e:
+    print("Помилка:", e)
 
-
-
-
+try:
+    fahrenheit = float(input("Введіть температуру в градусах Фаренгейта: "))
+    celsius = converter.fahrenheit_to_celsius(fahrenheit)
+    print(f"{fahrenheit} градусів Фаренгейта = {celsius} градусів Цельсія")
+except ValueError as e:
+    print("Помилка:", e)
